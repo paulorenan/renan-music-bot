578249a007f910b49a83c242e08cf70bf9170ef7
Paulo Renan Almeida <able.paulorenan@gmail.com>
2022-02-18 20:25:24 -0300

    configuração inicial

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const { Client } = require("discord.js");
+const { Player } = require("discord-player");
+
+const client = new Client({
+  restTimeOffset: 0,
+  shards: "auto",
+  intents: 641,
+});
+
+const player = new Player(client, {
+  leaveOnEmpty: true,
+  leaveOnEnd: true,
+  leaveOnStop: true,
+  leaveOnEmptyCooldown: 5000,
+  autoSelfDeaf: true,
+  initialVolume: 50,
+  bufferingTimeout: 3000,
+});
+
+module.exports = {
+  client,
+  player,
+};
